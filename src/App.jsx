93ee875968566e0cb111fb5d052ac47ec76665f0
alebaqueroben <alejandrobaqueroben@hotmail.com>
2024-02-27 21:32:32 -0300

    Previa parcial

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+
+  return (
+    <>
+    <h1>Carga de estudiantes</h1>
+    <form></form>
+    <Card/>
+    </>
+  )
+}
+
+export default App
